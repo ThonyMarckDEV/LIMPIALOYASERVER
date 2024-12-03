d3be540b2e089c35575d7fd338f9cc88b94f89b1
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2024-12-03 13:28:09 -0500

    FIRST COMMIT

---
+++ PHP/UrlHelper.php
@@ -0,0 +1,8 @@
+<?php
+class UrlHelper {
+    const BASE_URL = 'https://gardening-toolkit-crude-bingo.trycloudflare.com/PHP/';
+
+    public function getBaseUrl() {
+        return self::BASE_URL;
+    }
+}
