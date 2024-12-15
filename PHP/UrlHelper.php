8165aafcc76885fba4bd50887f22ad9b7ac85c4f
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2024-12-15 00:08:30 -0500

    aaaa

--- PHP/UrlHelper.php
+++
@@ -1,8 +0,0 @@
-<?php
-class UrlHelper {
-    const BASE_URL = 'https://fear-adipex-bibliographic-carefully.trycloudflare.com/PHP/';
-
-    public function getBaseUrl() {
-        return self::BASE_URL;
-    }
-}
--- PHP/get_reportes_hechos.php
+++ PHP/get_reportes_hechos.php
@@ -2,11 +2,11 @@
 // Incluir la conexión a la base de datos
 include '../conexion.php';
 
-// Consultar los reportes aprobados (estado 'revisado') y unir con la tabla de usuarios para obtener el username
+// Consultar los reportes aprobados (estado 'hecho') y unir con la tabla de usuarios para obtener el username
 $query = "SELECT reportes.idReporte, reportes.descripcion,reportes.fecha, reportes.imagen_url, reportes.latitud, reportes.longitud, usuarios.username 
           FROM reportes 
           JOIN usuarios ON reportes.idUsuario = usuarios.idUsuario 
-          WHERE reportes.estado = 'revisado'";
+          WHERE reportes.estado = 'hecho'";
 $result = $conn->query($query);
 
 $reportes = array();
